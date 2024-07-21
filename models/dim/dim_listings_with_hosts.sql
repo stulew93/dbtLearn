00f6cc3cd3a364677f5197d35608ac4f4f02db28
Stuart Lewis <stuart.lewis3110@gmail.com>
2024-07-21 23:08:34 +0100

    End of section 9

---
+++ models/dim/dim_listings_with_hosts.sql
@@ -0,0 +1,28 @@
+WITH listings_cleansed AS (
+    SELECT
+        *
+    FROM
+        {{ ref('dim_listings_cleansed') }}
+),
+hosts_cleansed AS (
+    SELECT
+        *
+    FROM
+        {{ ref('dim_hosts_cleansed') }}
+)
+SELECT
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    l.host_id,
+    h.host_name,
+    h.is_superhost as host_is_superhost,
+    l.created_at,
+    GREATEST(l.updated_at, h.updated_at) as updated_at
+FROM
+    listings_cleansed l
+LEFT JOIN
+    hosts_cleansed h
+ON (h.host_id = l.host_id)
